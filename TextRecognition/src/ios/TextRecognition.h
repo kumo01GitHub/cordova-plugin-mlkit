fd2095ca302dc45b0c814327d7f920dddbbe391d
kumo01GitHub <kumo01@outlook.com>
2025-08-03 02:19:50 +0900

    feat: add TextRecognition

---
+++ TextRecognition/src/ios/TextRecognition.h
@@ -0,0 +1,10 @@
+/********* TextRecognition.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface TextRecognition : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
